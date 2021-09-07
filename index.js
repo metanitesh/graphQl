adfe88b90166713c2212c5cbd3667422f129f247
Nitesh Sharma <nitesh.grep@gmail.com>
2021-09-07 15:12:31 +0530

    Adding a simple query and resolver

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const gql = require('graphql-tag');
-const ApolloServer = require('apollo-server')
+const {ApolloServer} = require('apollo-server')
 
 const typeDefs = gql`
-  type: {
-    
+  type User {
+    name: String!
+    phone: String
+  }
+
+  type Query {
+    me: User!
   }
 `
+
+const resolvers = {
+  Query: {
+    me (){
+      return {
+        name: "Nitesh",
+        phone: "8744041044"
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers
+})
+
+server.listen(4000)
+.then(() => console.log('listen on 4000'))
